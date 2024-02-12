5d74a4ac663b135d5414ed08fb8eef8cab8c9ef2
Paul Lalonde <plalonde@acm.org>
2024-02-12 12:58:30 -0800

    Dispatch works

--- t9500.asm
+++ t9500.asm
@@ -4,14 +4,16 @@
 .include "./m328Pdef.inc"
 .list 
 
-#define handlertab 0x100
-#define tabsize 16
+#define DEBUG 0
+
+
+#define handlertab 0x400
+#define tabsize 12
 ;;;;;;;;;;;;
 ; We allocate some registers *just* for the interrupt handler.  
 ; r0-r16 are reserved for userland
 ; r17-r25 are reserved for interrupts
 .def longdelayreg = r10
-.def longdelayreg2 = r11
 .def delayreg = r12
 .def delayreg2 = r13
 .def temp = r15
@@ -26,13 +28,34 @@
 .def jumptab = r20 ; To load into Z 
 .def jumptab2 = r21 ; just to mark it in-use
 .def jtabmult = r22; // Hold the size of the jtab entry. 16 bytes each.
-.def rval16 = r23 ; Hold the value 16 for our interrupt handler
+.def tabsize_r = r23 ; Hold the value 16 for our interrupt handler
 .def itemp = r24
 .def itemp2 = r25
 
 ;;
 ;; r26 onwards are X, Y, Z registers
 
+
+.MACRO PRINT_BSS 
+	push xh
+	push xl
+	ldi XH, HIGH((@0-BSS)*2+BSSFILL)
+	ldi XL, LOW((@0-BSS)*2+BSSFILL)
+	call USART_print_bss
+	pop xh
+	pop xl
+.ENDM
+
+.DSEG ; Keep in sync with BSS at the bottom.  Ugly.
+BSSFILL:
+status_shadow: 
+.byte 2
+REG:
+.byte 24
+HelloWorld:
+.byte 16
+StatusChanged:
+.byte 0x500
 
 
 .CSEG
@@ -59,9 +82,22 @@ Init:
 	SBI		DDRB, 5 ;; Enable writes to the blinking light
 	call 	USART_Init
 
-	ldi		rval16, 16
-	ldi 	ZH, HIGH(handlertab)
-	ldi 	ZL, Low(handlertab)
+
+
+	; Initialize bss data
+	ldi		itemp2, 2 * (BSSEND - BSS)
+	ldi 	ZH, HIGH(2*BSS)
+	ldi 	ZL, LOW(2*BSS)
+	ldi 	YH, HIGH(BSSFILL)
+	ldi 	YL, LOW(BSSFILL)
+bssloop:
+	lpm		r1, Z+
+	ST		Y+, r1
+	dec 	itemp2
+	brne	bssloop
+	ldi		tabsize_r, tabsize
+	ldi 	XH, HIGH(handlertab)
+	ldi 	XL, LOW(handlertab)
 
 #if 1
 	lds		temp, PCMSK0
@@ -78,15 +114,74 @@ Init:
 
 	sei
 #endif
-	Jmp		Main
 
+	LDI ZH, HIGH(TABOFFSET)
+
+	PRINT_BSS hello
 Main:
-;	mov r1, rval16
-;	call USART_HexOut
-;	call Delay
-	jmp Main ; let the interrupt handler do all the work.
-	;jmp DoBlink ; A test we have a working system.
+	sei
+	push itemp
+	ldi itemp, 1
+	mov longdelayreg, itemp
+	pop itemp
+	cli
+	call LongDelay
+.if 0
+	lds r1, status_shadow ; (Status_Shadow - BSS)*2+ BSSFILL
+	call USART_HexOut_r1
+	mov r1, status
+	call USART_HexOut_r1
+	call Newline
+.endif
+	lds r1, status_shadow ; (Status_Shadow - BSS)*2+ BSSFILL
+	cp r1, status
+	breq Main
+	call USART_HexOut_r1
+	call Newline
 
+	PRINT_BSS Status
+	lds r1, status_shadow
+	call USART_HexOut_r1
+	mov r1, status
+	call USART_HexOut_r1
+	call Newline
+
+	sts status_shadow, status
+	;call DumpRegisters
+
+	jmp Main ; let the interrupt handler do all the work.
+	;jmp Blink ; A test we have a working system.
+
+DumpRegisters:
+	ldi tempH, 23
+	mov r2, tempH
+	ldi 	XH, HIGH((Registers-BSS)*2+BSSFILL)
+	ldi 	XL, LOW((Registers-BSS)*2+BSSFILL)
+	call DumpMemory
+	ret
+
+;; Memory at X for r2 (destructive) iterations; destroys r1
+
+; IN: X, r2
+; Destroyed: temp, temph, itemp, r1, X, r2
+DumpMemory:
+	LD r1, X+
+	call USART_HexOut_r1
+	LDI tempH, ' '
+	call USART_Transmit_tempH
+	dec r2
+	brne DumpMemory
+Newline:
+; IN: none
+; Destroyed: tempH
+	ldi tempH, 0xA
+	call USART_Transmit_tempH
+	LDI tempH, 0xD
+	call USART_Transmit_tempH
+	ret
+
+; IN: delayreg, delayreg2
+; Destroyed: delayreg, delayreg2
 Delay:
 	nop
 	nop
@@ -110,6 +205,8 @@ Delay:
 	brne	Delay
 	ret
 	
+; IN: longdelayreg
+; Destroyed: delayreg, delayreg2, longdelayreg
 LongDelay:
 	eor		delayreg, delayreg
 	eor		delayreg2, delayreg2
@@ -118,13 +215,8 @@ LongDelay:
 	brne 	LongDelay
 	ret
 
-DoBlink:
-	; Set PORTB as an output for this test
-; This initialization happens in Init and doesn't need repeating here.
-;	ldi tempH,0b00100000
-;	out DDRB,tempH
-;	out PortB,tempH
-
+; IN: 
+; Destroyed: tempH, longdelayreg, delayreg, delayreg2
 Blink:
 	sbi		PORTB, 5
 
@@ -138,27 +230,12 @@ Blink:
 	call	LongDelay
 
 	rjmp Blink
-	
-#if 0
-	sbi		PORTB, 5
-
-	eor 	delayreg, delayreg
-	eor		delayreg2, delayreg2
-	call	Delay
-	
-	cbi		PORTB, 5
-	eor 	delayreg, delayreg
-	eor		delayreg2, delayreg2
-	call	Delay
-
-;	rjmp Blink
-#endif
-
 
 ;;
 ;; UART OUTPUT
 ;;
-
+; IN: none
+; Destroyed: tempH
 USART_Init:
 	; Set baud rate UBBRR = 16000000/(16*9600)-1
 	ldi tempH, 0 ; UBRRH_VALUE
@@ -173,110 +250,166 @@ USART_Init:
 	sts UCSR0C,tempH
 	ret	
 
-USART_Transmit:
+; IN: tempH
+; Destroyed:
+; Out: None
+;
+USART_Transmit_tempH:
+	push temp
+	sei
+USART_Transmit_tempH_loop:
 	; Wait for empty transmit buffer
 	lds temp, UCSR0A
 	SBRS temp,UDRE0
-	rjmp USART_Transmit
+	rjmp USART_Transmit_tempH_loop
 	; Put data (tempH) into buffer, sends the data 
 	sts UDR0,tempH
+	cli
+	pop temp
 	ret
 
-
+; IN: tempH
+; Destroyed: temp, tempH, itemp
+; Out: None
+;
+USART_HexNibbleOut_tempH:
+	andi tempH, 0xF
+	CPI tempH, 10
+	BRLO USART_HexNibbleOut_tempH_Digit
+	; Hex digit.  We will add '0' below, so the constant is a bit odd
+	ldi itemp, 'A' - '0' - 10
+	add tempH, itemp
+USART_HexNibbleOut_tempH_Digit:
+	ldi itemp, '0'
+	add tempH, itemp
+	call USART_Transmit_tempH
+	ret
 
 ; put r1 register to output as hex.
-USART_HexOut:
+;
+; IN: r1
+; Destroyed: tempH, itemp
+; Out: None
+;
+USART_HexOut_r1:
+;	cli
+	push tempH
+	mov tempH, r1
+	lsr tempH
+	lsr tempH
+	lsr tempH
+	lsr tempH
+	call USART_HexNibbleOut_tempH
 	mov tempH, r1
 	andi tempH, 0xF
-	ldi ZH, HIGH(2*HEX)
-	ldi ZL, LOW(2*HEX)
-	add ZL, tempH
-	adc ZH, r0
-	lpm tempH, Z
-	call USART_Transmit
-	lsr r1
-	lsr r1
-	lsr r1
-	lsr r1
-	ldi ZH, HIGH(2*HEX)
-	ldi ZL, LOW(2*HEX)
-	add ZL, r1
-	adc ZH, r0
-	lpm tempH, Z
-	call USART_Transmit
-	ldi ZH, HIGH(handlertab)
-	ldi ZL, LOW(handlertab)
+	call USART_HexNibbleOut_tempH
+	pop tempH
+;	sei
 	ret
 
-HEX:
-.db "0123456789abcdef"
+; IN: Z
+; Destroyed: XL, XH
+; Out: None
+USART_print_bss: 
+	push tempH
+USART_print_bss_loop:
+	ld tempH, X+
+	cpi tempH, 0 
+	breq USART_print_bss_done
+	call USART_Transmit_tempH
+	jmp USART_print_bss_loop
+USART_print_bss_done:
+	pop tempH
+	ret
+
+
 
 PCINT0_handler:
-	;eor		itemp2, itemp2
-	;sbr		itemp2, 1 << 5
-	;in		itemp, PORTB
-	;eor		itemp, itemp2
-	;out		PORTB, itemp
-ldi tempH, '*'
-call USART_Transmit
-;	reti
-
 	; Read the lines.  We sort out whether CS was high or low after
 	; doing the urgent thing.  CS going high means we don't commit.
 
 	in	itemp2, PORTD ;; If it was a data write, we needed to grab it, stat.
-	in	itemp, PORTB  ;; Ditto for our address lines.
-	cbr itemp, 1 << 5 ;; Don't look at that bit...
+	in	itemp, PINB  ;; Ditto for our address lines.
+	cbr itemp, 0b11110000 ;; Don't look at that bit...
+
 	; Now we jump to our handler
-	eor itemp, itemp
-	sbr itemp, 2
-	mul itemp, rval16  ;; Yes, we burn a register to hold "16"
-	mov r1, itemp
-	call USART_HexOut
-	mov ZL, itemp
+	mul itemp, tabsize_r  ;; Yes, we burn a register to hold the multiplier
+		;; Result in r1:r0
+
+	; Done in init. 
+	; LDI ZH, HIGH(TABOFFSET)
+
+	mov ZL, r0
+
 	ijmp
 
+
 .MACRO toggle
-in	itemp, PORTB
-eor itemp2, itemp2
-sbr itemp2, 1 << 5
-eor itemp, itemp2
-out PORTB, itemp
-	reti
+mov status, ZL
 .ENDMACRO
 .cseg
 .org handlertab + 0 * tabsize
-	eor itemp, itemp
-	call Delay
+TABOFFSET:
 	toggle
+	reti
 .org handlertab + 1 * tabsize
 	toggle
+	reti
 .org handlertab + 2 * tabsize
 	toggle
+	reti
 .org handlertab + 3 * tabsize
 	toggle
+	reti
 .org handlertab + 4 * tabsize
 	toggle
+	reti
 .org handlertab + 5 * tabsize
 	toggle
+	reti
 .org handlertab + 6 * tabsize
 	toggle
+	reti
 .org handlertab + 7 * tabsize
 	toggle
+	reti
 .org handlertab + 8 * tabsize
 	toggle
+	reti
 .org handlertab + 9 * tabsize
 	toggle
+	reti
 .org handlertab + 10 * tabsize
 	toggle
+	reti
 .org handlertab + 11 * tabsize
 	toggle
+	reti
 .org handlertab + 12 * tabsize
 	toggle
+	reti
 .org handlertab + 13 * tabsize
 	toggle
+	reti
 .org handlertab + 14 * tabsize
 	toggle
+	reti
 .org handlertab + 15 * tabsize
-	toggle
+mov status, ZL
+	reti
 
+.cseg
+BSS:
+StatusShadow:
+.db 0,0
+Registers:
+.db "abcdefghijklmnopqrstuvw",0
+hello:
+.db "hello, world!", 0xA, 0xD, 0
+Status:
+.db "Status changed: ", 0, 0
+BSSEND:
+
+.if 2*(BSSEND-BSS) > 255
+.error "BSS too large for single byte copy counter"
+.endif
